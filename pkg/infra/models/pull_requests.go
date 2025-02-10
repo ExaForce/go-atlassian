d67a79f07aeabaf44f4fd1a74829e6f106e6bee0
akash-exaforce <akash@exaforce.com>
2025-02-10 14:49:43 +0530

    Add List Repo Deploy Keys

--- pkg/infra/models/pull_requests.go
+++ pkg/infra/models/pull_requests.go
@@ -1,5 +1,25 @@
 package models
 
+type DeployKeysPageScheme struct {
+	Page     int                `json:"page"`
+	Pagelen  int                `json:"pagelen"`
+	Next     string             `json:"next"`
+	Previous string             `json:"previous"`
+	Values   []*DeployKeyScheme `json:"values"`
+}
+
+type DeployKeyScheme struct {
+	Id         int                     `json:"id"`
+	Key        string                  `json:"key"`
+	Repository *RepositoryScheme       `json:"repository"`
+	Comment    string                  `json:"comment"`
+	Label      string                  `json:"label"`
+	Type       string                  `json:"type"`
+	CreatedOn  string                  `json:"created_on"`
+	LastUsed   string                  `json:"last_used"`
+	Owner      *BitbucketAccountScheme `json:"owner"`
+}
+
 // PullRequestsResponse represents the response for a list of pull requests
 type PullRequestsResponse struct {
 	Size     int                  `json:"size"`
